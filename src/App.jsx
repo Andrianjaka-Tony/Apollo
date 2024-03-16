44a706e079861bb9eb64fdc5383d0e2c6c7b6929
Tony Rakotondrazaka ANDRIANJAKATSIHOARANA <andrianjaka.tony@gmail.com>
2024-03-16 15:47:42 +0300

    Hello world

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.scss";
+import useLenis from "./hooks/useLenis";
+
+function App() {
+  useLenis();
+
+  return <div className="App">Hello from HIU</div>;
+}
+
+export default App;
---
+++ src/hooks/useLenis.js
@@ -0,0 +1,10 @@
+import Lenis from "@studio-freight/lenis";
+
+export default function useLenis() {
+  const lenis = new Lenis();
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
+}
